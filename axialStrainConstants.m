fdf2d8059a8cc25f14ad1d18919b2665936cd210
Miles Skinner <skinner.miles@gmail.com>
2019-07-15 13:20:23 -0600

    Changes

--- axialStrainConstants.m
+++ axialStrainConstants.m
@@ -2,9 +2,6 @@ function [e0, e1] = axialStrainConstants(sigR, b,k, kappa)
 
 global mat rim w;
 
-
-
-
 fw = (1/4) * mat.rho * w^2 * eta0 * r^4;
 f1 = (1/3) * eta3 * e1 * r^3;
 f0 = (1/2) * eta4 * e0 * r^2;
@@ -16,4 +13,7 @@ m1 = (1/4) * eta3 * e1 * r^4;
 m0 = (1/3) * eta4 * e0 * r^3;
 
 m = mw + m1 + m0 + (1/(k+2))*eta1*r^(k+2) - (1/(k+2))*eta2*r^(-k+2);
+
+
+
 
