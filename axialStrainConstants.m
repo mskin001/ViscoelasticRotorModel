5506a9267822cb0acd50ad520459f62b41650b2f
Miles Skinner <skinner.miles@gmail.com>
2019-07-16 15:00:43 -0600

    Update axialStrainConstants.m
    changed back to Ha's 2 coefficient method. Solved for each coefficient

--- axialStrainConstants.m
+++ axialStrainConstants.m
@@ -2,21 +2,27 @@ function [e0] = axialStrainConstants(b,k)
 
 global mat rim w;
 
-syms r C1 C2 e0;
+syms r C1 C2 e0 e1;
+F = 0;
 
 [Q, kappa, fi] = findMatPropConsts(b,k);
+  
+ur = -mat.rho*w^2*fi(1)*r^3 + fi(2)*r^kappa + fi(3)*r^-kappa + fi(4)*eb*r^2 + fi(5)*ea*r;
 
-for a = 1:length(rim) - 1
+eTheta = ur/r;
+er = diff(ur,r);
 
-  uz = -(mat.rho{a}*w^2*r^3)/((9-kappa^2)*Q(3,3)) + C1*fi(2)*r^kappa + C2*fi(3)*r^-kappa...
-    + ((Q(2,2)-Q(3,1))/(Q(3,3)-Q(1,2)))*e0*r;
-  
-  eTheta = uz/r;
-  er = diff(uz,r);
-  
-  sigZ = Q(2,1)*eTheta + Q(2,2)*e0 + Q(2,3)*er;
-  sigZr = sigZ*r;
-  
-  fz = int(sigZr,r);
-  
-end
+sigZ = Q(2,1)*eTheta + Q(2,2)*ez + Q(2,3)*er;
+zr = sigZ*r;
+m = sigZ*r^2;
+
+fz = int(zr,r,rim(k),rim(k+1));
+mz = int(m,r,rim(k),rim(k+1));
+
+F = F + fz;
+M = M + mz;
+
+e0 = solve(F,ea);
+subs(M,ea,e0);
+e1 = solve(M,eb);
+subs(e0,eb,e1);
