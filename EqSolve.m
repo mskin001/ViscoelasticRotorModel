9acc27685291b4b8ac6a82ffda68feff7d49621e
Miles Skinner <skinner.miles@gmail.com>
2019-07-22 11:42:35 -0600

    Update EqSolve.m

--- EqSolve.m
+++ EqSolve.m
@@ -1,4 +1,4 @@
-syms b a p r
+syms b a p r x
 
 C = (a^2*p)/(2*(b^2 - a^2));
 A = (-p - 2*C)*a^2;
@@ -7,3 +7,10 @@ A = simplify(A);
 
 sigRR = A/r^2 + 2*C;
 sigRR = simplify(sigRR)
+
+k = diag(5,5).*1
+f = ones(5,1);
+
+f = f + (x^2);
+
+u = k\f;
