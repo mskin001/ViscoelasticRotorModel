60a12f6b5b9380a80125fc84aa1a96de6fcd7cdb
Miles Skinner <skinner.miles@gmail.com>
2019-07-30 10:20:22 -0600

    Update main.m

--- main.m
+++ main.m
@@ -16,16 +16,17 @@ global mat plotWhat
   % pe = steady state perfectly elastic
   % ve = steady state viscoelastic
   % qdve = quasi-dynamic viscoelasticu
-st = 've';
+st = 'pe';
 
 % Rotor
 % rim = [0.03789; 0.07901]; % single rim Ha 1999
+rim = [50/1000; 100/1000];
 % rim = [0.03786, 0.08393, 0.14707];
-rim = [0.0762, .1524]; % Tzeng2001
+% rim = [0.0762, .1524]; % Tzeng2001
 rdiv = 30; % number of points per rim to analyze
 delta = [0]/1000; % [mm]
 sigb = [0, 0];
-mats = {'IM7_8552_Tzeng2001.mat'};
+mats = {'Glass_Epoxy_Ha1999.mat'};
 % mats = {'AS_H3501_Ha1999.mat'; 'IM6_Epoxy_Ha1999.mat'};
 
 % Time/creep
@@ -35,7 +36,7 @@ numberOfSteps = 3;
 compFunc = @IM7_8552_Tzeng2001; % compliance function, input 'no' to turn off creep modeling
 
 % Speed/velocity
-rpm = 50000;
+rpm = 60000;
 vdiv = 1; % number of points to analyze between each fixed velocity
 
 % Plotting
