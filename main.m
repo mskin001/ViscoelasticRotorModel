1c9aea31db1e2c8d5c87d8c044f52d1ba09a3d6d
Miles Skinner <skinner.miles@gmail.com>
2019-10-28 20:25:28 -0600

    fix problems

--- main.m
+++ main.m
@@ -20,13 +20,13 @@ st = 'pe';
 
 % Rotor
 % rim = [0.03789; 0.07901]; % single rim Ha 1999
-% rim = [50/1000; 100/1000];
-rim = [0.110, .170, .2];
+% rim = [0.110, 0.2];
+rim = [.10, .110, 0.17, .2];
 % rim = [0.0762, .1524]; % Tzeng2001
 rdiv = 30; % number of points per rim to analyze
-delta = [0,0]/1000; % [mm]
+delta = [0.399, 0.413, 0]/1000; % [mm]
 sigb = [0, 0];
-mats = {'Glass_Epoxy_Ha1999.mat', 'IM6_Epoxy_Ha1999.mat'};
+mats = {'Alumin_7075_t6.mat','Glass_Epoxy_Ha1999.mat', 'AS4_HexTow.mat'};
 % mats = {'AS_H3501_Ha1999.mat'; 'IM6_Epoxy_Ha1999.mat'};
 
 % Time/creep
@@ -36,7 +36,7 @@ numberOfSteps = 3;
 compFunc = @IM7_8552_Tzeng2001; % compliance function, input 'no' to turn off creep modeling
 
 % Speed/velocity
-rpm = 50000;
+rpm = 20000;
 vdiv = 1; % number of points to analyze between each fixed velocity
 
 % Plotting
