31109ee7ec0ede8806859bfad907185caa390cd9
Miles Skinner <skinner.miles@gmail.com>
2020-02-03 17:02:40 -0700

    update

--- test.m
+++ test.m
@@ -1,37 +1,26 @@
-Et = 38.6e9;
-Ez = 8.27e9;
-Er = Ez;
+t = linspace(1,3400,3400);
+t0 = 0;
 
-Gtz = 4.14e9;
-Gtr = Gtz;
-Grz = Gtz;
+c1 = 33.3e-5;
+c2 = 1.8391e-10;
+c3 = 5.312e-7;
+c4 = 1.6575e-6;
+str = 5;
 
-nutz = 0.26;
-nutr = nutz;
-nurz = nutz;
+j = c1 + c2*(t-t0) + c3 * (1-exp((t0-t)*(c4/c3)));
 
-Qha = [1/Et -nutr/Er -nutz/Ez;
-       -nutr/Er 1/Er -nurz/Ez;
-       -nutz/Ez -nurz/Ez 1/Ez]^-1
-     
-     
-load('MaterialProperties/Glass_Epoxy_Ha1999.mat')
-E11 = mstiff(1);
-E22 = mstiff(2);
-G12 = mstiff(3);
-G23 = mstiff(4); % Only used if nu23 is not specified
-nu12 = mstiff(5);
-nu23 = nu12; % E22/(2*G23)-1;
+elong = str * j;
 
-S11 = 1/E11;
-S22 = 1/E22;
-S33 = S22;
-G12 = 1/G12;
+h1 = 31.2;
+matC1 = 4.11e-9;
+matC2 = 1.82;
+matD1 = -11.90;
+matD2 = 1.6776;
+matStr = [10.83 12.48 14.13 16.27 18.32 19.58];
 
-S = [ S11    -nu12*S22 -nu12*S33 0;
-     -nu12*S22 S22     -nu23*S33 0;
-     -nu12*S33 -nu23*S22  S33 0;
-     0 0 0 G12];
-Q = inv(S);
-% Q(1:2:3,1:2:3) = Q(1:2:3,1:2:3)-Q(1:2:3,2:2:4)*...
-%     inv(Q(2:2:4,2:2:4))*Q(2:2:4,1:2:3)
+for k = 1:length(matStr)
+  h2 = log(1+h1) / (matD1 * matStr(k)^(matD2-1));
+  eMat(k,1:length(t)) = (1/(h2*matStr(k))) * log(1 + (1+h1) * exp(h2*matC1*matStr(k)^(matC2+1)*t) - 1);
+  S(k,1:length(t)) = eMat(k)/matStr(k);
+  E(k,1:length(t)) = S(k,1:length(t)).^-1;
+end
