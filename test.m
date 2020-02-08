62981d1de53e1aa455f1ae0936ba518aac7b2d07
Miles Skinner <skinner.miles@gmail.com>
2020-02-08 16:23:46 -0700

    update

--- test.m
+++ test.m
@@ -1,37 +1,16 @@
-Et = 38.6e9;
-Ez = 8.27e9;
-Er = Ez;
 
-Gtz = 4.14e9;
-Gtr = Gtz;
-Grz = Gtz;
+t = linspace(0,4700,4700);
+t0 = zeros(1,length(t));
+sigma = 79.25;
 
-nutz = 0.26;
-nutr = nutz;
-nurz = nutz;
+c1 = 3.3333e-5;
+c2 = 1.8391e-10;
+c3 = 5.3121e-7;
+c4 = 1.6575e-6;
 
-Qha = [1/Et -nutr/Er -nutz/Ez;
-       -nutr/Er 1/Er -nurz/Ez;
-       -nutz/Ez -nurz/Ez 1/Ez]^-1
-     
-     
-load('MaterialProperties/Glass_Epoxy_Ha1999.mat')
-E11 = mstiff(1);
-E22 = mstiff(2);
-G12 = mstiff(3);
-G23 = mstiff(4); % Only used if nu23 is not specified
-nu12 = mstiff(5);
-nu23 = nu12; % E22/(2*G23)-1;
+j = c1 + c2.*(t - t0) + c3*(1 - exp((t0-t)*(c4/c3)));
 
-S11 = 1/E11;
-S22 = 1/E22;
-S33 = S22;
-G12 = 1/G12;
+e = sigma * j;
 
-S = [ S11    -nu12*S22 -nu12*S33 0;
-     -nu12*S22 S22     -nu23*S33 0;
-     -nu12*S33 -nu23*S22  S33 0;
-     0 0 0 G12];
-Q = inv(S);
-% Q(1:2:3,1:2:3) = Q(1:2:3,1:2:3)-Q(1:2:3,2:2:4)*...
-%     inv(Q(2:2:4,2:2:4))*Q(2:2:4,1:2:3)
+strain = [e(1), e(100), e(1000), e(2500), e(4700)];
+disp(strain)
